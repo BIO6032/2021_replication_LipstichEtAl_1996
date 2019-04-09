e7b1a126faac72121281033b4972f96ca2b35ada
Sandrine <sandrine.soeharjono@umontreal.ca>
2019-04-09 14:37:45 -0400

    Update call_upon_newY.jl

--- call_upon_newY.jl
+++ call_upon_newY.jl
@@ -3,9 +3,9 @@ include("Fig1_strain_parameters.jl")
 function thousand_para()
     ux = 0.2;
     for k in 1:1000
-        array = zeros((1000,4))
+        new = zeros((1000,4))
         b = new_Y()
-        array[k,:] = [b.uy, b.by, b.ey, b.βy]
+        new[k,:] = [b.uy, b.by, b.ey, b.βy]
         return array
     end
 end
