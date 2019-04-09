419d1f75edfb5524f49fc4b197239692256bc8df
kiristern <kiri.a.stern@gmail.com>
2019-04-09 14:42:56 -0400

    clean up script, add comments

--- simulate_parasite_dynamics.jl
+++ simulate_parasite_dynamics.jl
@@ -1,13 +1,11 @@
 using Plots
 include("Fig1_strain_parameters.jl")
 
-
 #using fonctions defined in Fig1_strain_parameters
-#parameters
+# making a new parasite & a new host
 p = new_Y()
 h = new_X()
 #model
 N = parasites(h, p, 100.0, 20.0, timesteps = 5000, iter = 1)
 #graph
-using Plots
 plot(N, 1, 5001, labels = ["X", "Y"], title = "Dynamics of populations X and Y")
