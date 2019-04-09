94652b255e45d86e500551cb62c4d947b87a167f
kiristern <kiri.a.stern@gmail.com>
2019-04-09 12:51:16 -0400

    #7 added host function. called upon both functions (new_Y, new_X) to create p and h to input into parasite function and plot. **could not call upon this script from new script (simulate_parasite.jl)**

--- Fig1_strain_parameters.jl
+++ Fig1_strain_parameters.jl
@@ -12,6 +12,14 @@ struct host_X
     ex::Float64
 end
 
+function new_X()
+    ux = 0.2
+    bx = 1.0
+    ex = 50
+    new_host = host_X(ux, bx, ex)
+    return new_host
+end
+
 function new_Y()
     ux=0.2
     uy=rand(Float64)
@@ -23,29 +31,40 @@ function new_Y()
 end
 #newY = new_Y()             #calling the function once to test
 
-function parasites(xx::para_Y, X0::Float64, Y0::para_Y ; timesteps::Int64, iter::Int64)
+function parasites(xx::host_X, yy::para_Y, X0::Float64, Y0::Float64 ; timesteps::Int64, iter::Int64)
+    bx = 1.0;
+    c = 2.0;
     # calling the new_Y function (for 1000 strains)
     for g in 1:timesteps
         # calling the new_Y function in the container 1
-        densities = zeros(timesteps+1, 5)
+        densities = zeros(timesteps+1, 2)
         densities[1,:] = [X0, Y0]
         for i in 1:timesteps
             X = densities[i, 1]
             Y = densities[i, 2]
 
             for j in 1:iter
-                dXdt = X *(xx.bx*(1 - X - Y) - xx.ux - xx.c*(xx.βy*Y))
-                dYdt = Y *(xx.by * (1 - X - Y) - xx.uy + xx.c*xx.βy*X)
+                dXdt = X *(xx.bx*(1 - X - Y) - xx.ux - c*(yy.βy*Y))
+                dYdt = Y *(yy.by * (1 - X - Y) - yy.uy + c*yy.βy*X)
                 X = X + dXdt
                 Y = Y + dYdt
             end
-        end
         if all([X, Y] .> 0)
             densities[i+1,:] = [X, Y]
         else
             break
         end
     end
-
     return densities
 end
+end
+
+#using fonctions defined in Fig1_strain_parameters
+#parameters
+p = new_Y()
+h = new_X()
+#model
+N = parasites(h, p, 100.0, 10.0, timesteps = 1000, iter = 100)
+#graph
+using Plots
+plot(N, labels = ["X", "Y"])
