9a6e0ffb0e8bf230b0c2dd827c404bc49be01d09
kiristern <kiri.stern@umontreal.ca>
2019-04-09 12:01:55 -0400

    eidts to function definitions
    change indentations and removal of unnecessary lines

--- Fig1_strain_parameters.jl
+++ Fig1_strain_parameters.jl
@@ -1,6 +1,3 @@
-#include("Fig1_XY.jl")
-using Plots
-
 struct para_Y
     uy::Float64
     by::Float64
@@ -21,7 +18,6 @@ struct host_X
     ex::Float64
 end
 
-# function to create new Y strain (parasite machine)
 function new_Y()
     ux=0.2
     uy=rand(Float64)
@@ -33,30 +29,29 @@ function new_Y()
 end
 #newY = new_Y()             #calling the function once to test
 
-function parasites(xx::Para, X0::Float64, Y0::Float64 ; timesteps::Int64, iter::Int64)
-# calling the new_Y function (for 1000 strains)
-timesteps = 4
-for g in 1:timesteps
-X0 =
-Y0 = new_Y()
-# calling the new_Y function in the container 1
-densities = zeros(timesteps+1, 5)
-densities[1,:] = [X0, Y0]
-for i in 1:timesteps
-    X = densities[i, 1]
-    Y = densities[i, 2]
+function parasites(xx::para_Y, X0::Float64, Y0::para_Y ; timesteps::Int64, iter::Int64)
+    # calling the new_Y function (for 1000 strains)
+    for g in 1:timesteps
+        # calling the new_Y function in the container 1
+        densities = zeros(timesteps+1, 5)
+        densities[1,:] = [X0, Y0]
+        for i in 1:timesteps
+            X = densities[i, 1]
+            Y = densities[i, 2]
 
-    for j in 1:iter
-        dXdt = X *(xx.bx*(1 - X - Y) - xx.ux - xx.c*(xx.βy*Y))
-        dYdt = Y *(xx.by * (1 - X - Y) - xx.uy + xx.c*xx.βy*X)
-        X = X + dXdt
-        Y = Y + dYdt
+            for j in 1:iter
+                dXdt = X *(xx.bx*(1 - X - Y) - xx.ux - xx.c*(xx.βy*Y))
+                dYdt = Y *(xx.by * (1 - X - Y) - xx.uy + xx.c*xx.βy*X)
+                X = X + dXdt
+                Y = Y + dYdt
+            end
+        end
+        if all([X, Y] .> 0)
+            densities[i+1,:] = [X, Y]
+        else
+            break
+        end
     end
-end
-    if all([X, Y] .> 0)
-        densities[i+1,:] = [X, Y]
-    else
-        break
-    end
-return densities
+
+    return densities
 end
