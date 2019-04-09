a5a480db8342269c569a8907b07b3e99ef8f7140
kiri <kiri.stern@umontreal.ca>
2019-04-09 11:39:17 -0400

    copied Fares function and trying to add "parasite factory" #7

--- Fig1_strain_parameters.jl
+++ Fig1_strain_parameters.jl
@@ -8,6 +8,13 @@ struct para_Y
     βy::Float64
 end
 
+struct beta.y
+    r1::Float64
+    α::Float64
+    by::Float64
+    bx::Float64
+end
+
 struct host_X
     ux::Float64
     bx::Float64
@@ -25,3 +32,31 @@ function new_Y()
     return new_guy
 end
 #newY = new_Y()             #calling the function once to test
+
+function parasites(xx::Para, X0::Float64, Y0::Float64 ; timesteps::Int64, iter::Int64)
+# calling the new_Y function (for 1000 strains)
+timesteps = 4
+for g in 1:timesteps
+X0 =
+Y0 = new_Y()
+# calling the new_Y function in the container 1
+densities = zeros(timesteps+1, 5)
+densities[1,:] = [X0, Y0]
+for i in 1:timesteps
+    X = densities[i, 1]
+    Y = densities[i, 2]
+
+    for j in 1:iter
+        dXdt = X *(xx.bx*(1 - X - Y) - xx.ux - xx.c*(xx.βy*Y))
+        dYdt = Y *(xx.by * (1 - X - Y) - xx.uy + xx.c*xx.βy*X)
+        X = X + dXdt
+        Y = Y + dYdt
+    end
+end
+    if all([X, Y] .> 0)
+        densities[i+1,:] = [X, Y]
+    else
+        break
+    end
+return densities
+end
