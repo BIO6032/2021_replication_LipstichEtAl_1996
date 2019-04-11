08d05bfcf45e159143e4186e896bae122e296eea
Fares Dhane <fs.dhane@gmail.com>
2019-04-11 12:01:35 -0400

    Added for loop

--- Équation differentielle.jl
+++ Équation differentielle.jl
@@ -29,34 +29,40 @@ uy = rand(Float64)
 
 #newY = new_para()
 
-ey = fill(0.9, 100)    # constant for fig 2 part 1
-βy = fill(0.0, 100)
+ey = fill(0.0, 1000)    # constant for fig 2 part 1
+βy = fill(0.0, 1000)
 
 Y = zeros(Float64, length(ey))
-Y[1] = 10.0
+Y[1] = 5.0
 X0 = 1.0
 
-function fu(u, p, t)
-    x = u[1]
-    y = u[2:end]
-    regul = (1-(sum(u))/(p.K))
-    dx = (p.bx*x + sum(p.ey.*y))*regul - p.ux*x-p.c*sum(p.βy.*y)*x
-    dy = p.by .* y .* regul .- p.uy .* y .+ p.c .* p.βy .* x .* y
-    return vcat(dx, dy)
+#for i in 1:10
+    function fu(u, p, t)
+        x = u[1]
+        y = u[2:end]
+        regul = (1-(sum(u))/(p.K))
+        dx = (p.bx*x + sum(p.ey.*y))*regul - p.ux*x-p.c*sum(p.βy.*y)*x
+        dy = p.by .* y .* regul .- p.uy .* y .+ p.c .* p.βy .* x .* y
+        return vcat(dx, dy)
+    end
+
+    parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 20.0, ux = ux, by = by, uy = uy)
+    #i=1.0
+    debut = 0.0
+    fin = i .* 100.0
+    prob = ODEProblem(fu, vcat(X0, Y), (debut, fin), parameters)
+    solution = solve(prob)
+    debut = fin
+    plot(solution, leg=false)
+#end
+
+for i in 2:10
+    new_u = solution[end]
+    new_y = findfirst(x -> x == 0.0, new_u)
+    new_u[new_y] = 10
+    fin = i .* 100.00
+    prob = ODEProblem(fu, new_u, (debut, fin), parameters)
+    solution = solve(prob)
+    debut = fin
+    plot(solution, leg=false)
 end
-
-parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 20.0, ux = ux, by = by, uy = uy)
-
-prob = ODEProblem(fu, vcat(X0, Y), (0.0, 100.0), parameters)
-solution = solve(prob)
-plot(solution, leg=false)
-
-#new_u = solution[end]
-
-# new_y = findfirst(x -> x == 0.0, new_u)
-# new_u[new_y] = 1
-#
-# prob = ODEProblem(fu, new_u, (100.0, 200.0), parameters)
-# solution = solve(prob)
-#
-# plot(solution, leg=false)
