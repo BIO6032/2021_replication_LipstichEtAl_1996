323b6f4bd71b9ed3bf2c2e5837776d6422fff16c
kiristern <kiri.a.stern@gmail.com>
2019-04-23 15:57:01 -0400

    edited K to 80

--- Équation differentielle.jl
+++ Équation differentielle.jl
@@ -35,7 +35,7 @@ duree = 100.0
 fin = debut + duree
 N = zeros(Float64, (n_parasites+1, (n_parasites-1)*Int(duree)+1))
 new_U = vcat(X0, Y)
-parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 100.0, ux = ux, by = by, uy = uy)
+parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy = uy)
 
 # each strain introduction (1000x)
 @progress "Simulation" for i in 2:length(Y)
