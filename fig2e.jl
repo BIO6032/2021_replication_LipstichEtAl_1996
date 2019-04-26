8ddb324ac3277d9981922ae53f747c4b1f9db874
Fares Dhane <fs.dhane@gmail.com>
2019-04-26 12:35:09 -0400

    Figure 2 : e et f

---
+++ fig2e.jl
@@ -0,0 +1,4 @@
+#Figure 2 e pour by = 0.1
+
+Vertical_case1 = fill(0.1, 100001)
+plot(Vertical_case1, ylims=(0,1),leg=false,title = "Fraction of all new cases acquired vertically")
---
+++ fig2f.jl
@@ -0,0 +1,4 @@
+#figure 2 f pour by = 1.0
+
+Vertical_case2 = fill(1.0, 100001)
+plot(Vertical_case2, ylims=(0,1),leg=false, title = "Fraction of all new cases acquired vertically")
