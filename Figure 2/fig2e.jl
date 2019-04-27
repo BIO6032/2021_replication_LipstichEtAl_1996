6b24637a464f05549d1b68910d6b512f39278921
kiristern <kiri.a.stern@gmail.com>
2019-04-26 20:31:46 -0400

    add graph labels and save graph image

--- Figure 2/fig2e.jl
+++ Figure 2/fig2e.jl
@@ -1,4 +1,7 @@
 #Figure 2-e pour by = 0.1
 
 Vertical_case1 = fill(0.1, 100001)
-plot(Vertical_case1, ylims=(0,1),leg=false,title = "Fraction of all new cases acquired vertically")
+plot(Vertical_case1, ylims=(0,1),leg=false,title = "Fraction of all new cases acquired vertically",
+xlabel = "Time", ylabel = "Vertical cases")
+
+png("Figure 2/graph_2e.png")
