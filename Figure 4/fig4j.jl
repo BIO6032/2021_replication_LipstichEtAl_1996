29956b26c8d08067dd83322023f2f468e845d537
Fares Dhane <fs.dhane@gmail.com>
2019-04-28 16:59:09 -0400

    Figures i and j

---
+++ Figure 4/fig4j.jl
@@ -0,0 +1,11 @@
+#calculating evenness
+function pielou(n)
+    np = filter(x -> x > eps(), n)
+    p = np./sum(np)
+    ev = length(p) == 1 ? 0.0 : -sum(p.*log.(p))*(1/log(length(p)))
+    return ev
+end
+ev = mapslices(pielou, Np[:,2:end]; dims=2)
+plot(ev, title = "Evenness", xlabel = "Time", ylabel = "Relative abundance (log)", leg = false)
+
+png("Figure 4/graph_4j.png")
