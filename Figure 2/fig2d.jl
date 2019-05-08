7e0889c0c38b6acb262e3302c45e9d60719e3c48
faresdhane <fs.dhane@gmail.com>
2019-05-08 15:28:30 -0400

    Add fixed number and fixed Y values of the Mean R0 graph

--- Figure 2/fig2d.jl
+++ Figure 2/fig2d.jl
@@ -1,19 +1,21 @@
 using DifferentialEquations
 using Plots
+import Random
 
-n_parasites = 100
+n_parasites = 100;
 
 #parameters
 c = 4.0;
 ux = 0.2;
 ux1 = fill(0.2, n_parasites); #le ux et uy 1000 à cause de la β # une autre façon :[0.2 for x in 1:1000]
-uy = rand(200:1000, n_parasites)/1000 #doit être toujours >= à ux
+Random.seed!(1234);
+uy = rand(200:1000, n_parasites)/1000; #doit être toujours >= à ux
 u1 = (uy - ux1);
 βy = 3 * u1 ./ (u1.+ 1); # augmente avec la mortalité u
 bx = 1.0;
 by = 1.0;
-#ey = bx - by
-ey = fill(0.9, n_parasites);    # constant for fig 2 part 1
+
+ey = fill(0.9, n_parasites);    # constant for fig 2 part 1, ey = bx - by
 
 Y = zeros(Float64, length(ey));
 Y[1] = 1.0;
@@ -29,12 +31,12 @@ function fonction(u, p, t)
     return vcat(dx, dy)
 end
 
-debut = 0.0
-duree = 1000.0
-fin = debut + duree
-N = zeros(Float64, (n_parasites+1, (n_parasites-1)*Int(duree)+1))
-new_U = vcat(X0, Y)
-parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy = uy)
+debut = 0.0;
+duree = 1000.0;
+fin = debut + duree;
+N = zeros(Float64, (n_parasites+1, (n_parasites-1)*Int(duree)+1));
+new_U = vcat(X0, Y);
+parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy = uy);
 
 # each strain introduction (1000x)
 @progress "Simulation" for i in 2:length(Y)
@@ -43,7 +45,6 @@ parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy
     solution = solve(prob, saveat=debut:1.0:fin)
     for t in eachindex(solution.t)
         pop = solution.u[t]
-        uy_avg = avg_w_survived
         for i in 1:n_parasites
             if (pop.<0)[i]
                 pop[i] = 0
@@ -63,41 +64,37 @@ parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy
     global fin = debut + duree
 end
 
-Np = N'
+Np = N';
 
 #strains that survive at each time step for uy
-survival = (Np.>0.0)[:,2:end]
-survived_ui = survival.*uy'
-avg_survived = sum(survived_ui; dims=2)./sum(survival; dims=2)
-avg_w_survived = sum(Np[:,2:end].*uy'; dims=2)./sum(Np[:,2:end]; dims=2)
-uy_avg = avg_w_survived
+survival = (Np.>0.0)[:,2:end];
+survived_ui = survival.*uy';
+avg_survived = sum(survived_ui; dims=2)./sum(survival; dims=2);
+avg_w_survived = sum(Np[:,2:end].*uy'; dims=2)./sum(Np[:,2:end]; dims=2);
+uy_avg = avg_w_survived;
 
 #strains that survive for βy
-survived_βy = survival.*βy'
-avg_survived_βy = sum(survived_βy; dims=2)./sum(survival; dims=2)
-βi_avg = avg_survived_βy
+survived_βy = survival.*βy';
+avg_survived_βy = sum(survived_βy; dims=2)./sum(survival; dims=2);
+βi_avg = avg_survived_βy;
 
 #avec moins de bruit (weighted)
-avg_w_survived_βy = sum(Np[:,2:end].*βy'; dims=2)./sum(Np[:,2:end]; dims=2)
-βy_avg = avg_w_survived_βy
+avg_w_survived_βy = sum(Np[:,2:end].*βy'; dims=2)./sum(Np[:,2:end]; dims=2);
+βy_avg = avg_w_survived_βy;
 
 #calculating R0
-k=1
-H0 = c*βi_avg./uy_avg.*k.*(1-ux/bx)
+k=1;
+H0 = c*βi_avg./uy_avg.*k.*(1-ux/bx);
 
-H0_w = c*βy_avg./uy_avg.*k.*(1-ux/bx)
+H0_w = c*βy_avg./uy_avg.*k.*(1-ux/bx);
 
-V0 = by*ux./(bx*uy_avg)
+V0 = by*ux./(bx*uy_avg);
 
-R0 = H0 + V0
+R0 = H0 + V0;
 
-R0_w = H0_w + V0
+R0_w = H0_w + V0;
 
-plot(R0, title = "Average R0 in the population", xlabel = "Time", ylabel = "Mean R0", leg = false)
-plot!(R0_w)
+# plot(R0, title = "Average R0 in the population", xlabel = "Time", ylabel = "Mean R0", leg = false, ylims=(0,))
+plot(R0_w,c=:black, title = "Average R0 in the population", xlabel = "Time", ylabel = "Mean R0", leg = false, ylims=(0,7))
 
-png("Figure 2/graph_2d")
-
-# values of H0 and V0
-H0
-V0
+# png("Figure 2/graph_2d")
