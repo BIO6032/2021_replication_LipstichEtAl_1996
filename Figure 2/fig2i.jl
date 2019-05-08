12390c217bcf660ef0346d690770d7857867c143
faresdhane <fs.dhane@gmail.com>
2019-05-08 15:58:04 -0400

    Recap, new and FINAL graphs

--- Figure 2/fig2i.jl
+++ Figure 2/fig2i.jl
@@ -7,8 +7,8 @@ function pielou(n)
     p = np./sum(np)
     ev = length(p) == 1 ? 0.0 : -sum(p.*log.(p))*(1/log(length(p)))
     return ev
-end
-ev = mapslices(pielou, Np[:,2:end]; dims=2)
-plot(ev, title = "Evenness", xlabel = "Time", ylabel = "Relative abundance (log)", leg = false)
+end;
+ev = mapslices(pielou, Np[:,2:end]; dims=2);
+plot(ev, c=:black, title = "Evenness", xlabel = "Time", ylabel = "Relative abundance (log)", leg = false, ylims = (0,1))
 
-png("Figure 2/graph_2i.png")
+# png("Figure 2/graph_2i.png")
