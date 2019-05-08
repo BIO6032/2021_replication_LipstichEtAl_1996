5c4f7e55ebaba9aa8a434079012dc024b0e9b8ea
faresdhane <fs.dhane@gmail.com>
2019-05-08 15:50:06 -0400

    Add fixed random number and the graphs of uy

--- Figure 2/fig2h.jl
+++ Figure 2/fig2h.jl
@@ -1,13 +1,15 @@
 using DifferentialEquations
 using Plots
+import Random
 
-n_parasites = 100
+n_parasites = 100;
 
 #Figure 2
 c = 4.0;
 ux = 0.2;
 ux1 = fill(0.2, n_parasites); #le ux et uy 1000 à cause de la β # une autre façon :[0.2 for x in 1:1000]
-uy = rand(200:1000, n_parasites)/1000 #doit être toujours >= à ux
+Random.seed!(1234);
+uy = rand(200:1000, n_parasites)/1000; #doit être toujours >= à ux
 u1 = (uy - ux1);
 βy = 3 * u1 ./ (u1.+ 1); # augmente avec la mortalité u
 bx = 1.0;
@@ -29,12 +31,12 @@ function fonction(u, p, t)
     return vcat(dx, dy)
 end
 
-debut = 0.0
-duree = 1000.0
-fin = debut + duree
-N = zeros(Float64, (n_parasites+1, (n_parasites-1)*Int(duree)+1))
-new_U = vcat(X0, Y)
-parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy = uy)
+debut = 0.0;
+duree = 1000.0;
+fin = debut + duree;
+N = zeros(Float64, (n_parasites+1, (n_parasites-1)*Int(duree)+1));
+new_U = vcat(X0, Y);
+parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy = uy);
 
 # each strain introduction (1000x)
 @progress "Simulation" for i in 2:length(Y)
@@ -62,17 +64,18 @@ parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy
     global fin = debut + duree
 end
 
-Np = N'
+Np = N';
 
 #strains that survive at each time step
-survival = (Np.>0.0)[:,2:end]
-survived_ui = survival.*uy'
-avg_survived = sum(survived_ui; dims=2)./sum(survival; dims=2)
+survival = (Np.>0.0)[:,2:end];
+survived_ui = survival.*uy';
+avg_survived = sum(survived_ui; dims=2)./sum(survival; dims=2);
 
-avg_w_survived = sum(Np[:,2:end].*uy'; dims=2)./sum(Np[:,2:end]; dims=2)
+avg_w_survived = sum(Np[:,2:end].*uy'; dims=2)./sum(Np[:,2:end]; dims=2);
 
-plot(avg_survived, title = "Average uy in the population",
-    xlabel = "Time", ylabel = "Mean mortality (uy)", leg = false)
-plot!(avg_w_survived)
+# plot(avg_survived, title = "Average uy in the population",xlabel = "Time", ylabel = "Mean mortality (uy)", leg = false)
+plot(avg_w_survived, c=:black,title = "Average uy in the population",
+    xlabel = "Time", ylabel = "Mean mortality (uy)", leg = false, ylims =(0,1))
 
-png("Figure 2/graph_2h.png")
+
+# png("Figure 2/graph_2h.png")
