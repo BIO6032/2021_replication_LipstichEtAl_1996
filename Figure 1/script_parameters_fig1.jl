9e317af1b83e9f429ba3a8d89f20960e95c0a5fc
faresdhane <fs.dhane@gmail.com>
2020-08-31 22:16:57 -0400

    "ey" and "uy" to "ei" and "ui"

--- Figure 1/script_parameters_fig1.jl
+++ Figure 1/script_parameters_fig1.jl
@@ -8,18 +8,18 @@ n_parasites = 100
 
 c = 4.0
 ux = 0.2;
-ux1 = fill(0.2, n_parasites); #le ux et uy 1000 à cause de la β # une autre façon :[0.2 for x in 1:1000]
-uy = rand(200:1000, n_parasites)/1000 #doit être toujours >= à ux
-u1 = (uy - ux1);
-βy = 3 * u1 ./ (u1.+ 1); # augmente avec la mortalité u
+ux1 = fill(0.2, n_parasites); #ux is ui 1000 because of β //different way to have the same results :[0.2 for x in 1:1000]
+ui = rand(200:1000, n_parasites)/1000 # must always be >= à ux
+u1 = (ui - ux1);
+βy = 3 * u1 ./ (u1.+ 1); # increases with mortality u
 bx = 1.0;
-ey = fill(0.9, n_parasites);    # constant for fig 2 part 1
+ei = fill(0.9, n_parasites);    # constant for fig 2 part 1
 debut = 0.0
 duree = 1000.0
 fin = debut + duree
 N = zeros(Float64, (n_parasites+1, (n_parasites-1)*Int(duree)+1))
 new_U = vcat(X0, Y)
-parameters = (bx = bx, βy = βy, ey = ey, c = c, K = 80.0, ux = ux, by = by, uy = uy)
+parameters = (bx = bx, βy = βy, ei = ei, c = c, K = 80.0, ux = ux, by = by, ui = ui)
 
 Y = zeros(Float64, length(ey));
 Y[1] = 1.0;
