73bade7d6cab1a379cb2abc90bc4a0059b2a8a83
faresdhane <fs.dhane@gmail.com>
2020-08-31 22:17:35 -0400

    "ey" and "uy" to "ei" and "ui"

--- Figure 1/fig1acegi.jl
+++ Figure 1/fig1acegi.jl
@@ -22,15 +22,15 @@ plot!(Np[:,1], c=:black, lw=0.4, label = "Uninfected")
 
 ###### FIGURE 1g ######
 
-#strains that survive at each time step for uy
+#strains that survive at each time step for ui
 survival = (Np.>0.0)[:,2:end];
-survived_ui = survival.*uy';
+survived_ui = survival.*ui';
 avg_survived = sum(survived_ui; dims=2)./sum(survival; dims=2);
 
-avg_w_survived = sum(Np[:,2:end].*uy'; dims=2)./sum(Np[:,2:end]; dims=2);
-uy_avg = avg_w_survived;
+avg_w_survived = sum(Np[:,2:end].*ui'; dims=2)./sum(Np[:,2:end]; dims=2);
+ui_avg = avg_w_survived;
 
-# plot(avg_survived, title = "Average uy in the population",xlabel = "Time", ylabel = "Mean mortality (ui)", leg = false)
+# plot(avg_survived, title = "Average ui in the population",xlabel = "Time", ylabel = "Mean mortality (ui)", leg = false)
 plot(avg_w_survived, c=:black,title = "Average mortality in the population",
     xlabel = "Time", ylabel = "Mean mortality (ui)", leg = false, ylims =(0,1))
 
@@ -50,11 +50,11 @@ avg_w_survived_βy = sum(Np[:,2:end].*βy'; dims=2)./sum(Np[:,2:end]; dims=2);
 βy_avg = avg_w_survived_βy;
 
 k=1;
-H0 = c*βi_avg./uy_avg.*k.*(1-ux/bx);
+H0 = c*βi_avg./ui_avg.*k.*(1-ux/bx);
 
-H0_w = c*βy_avg./uy_avg.*k.*(1-ux/bx);
+H0_w = c*βy_avg./ui_avg.*k.*(1-ux/bx);
 
-V0 = by*ux./(bx*uy_avg);
+V0 = by*ux./(bx*ui_avg);
 
 plot(V0, c=:black, title = "Average vertical transmission in the population", xlabel = "Time", ylabel = "Mean R0", leg = false, ylims=(0,1))
 
