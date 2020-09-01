1b6734105db062319197aab2f6c9cf4b75e8f944
faresdhane <fs.dhane@gmail.com>
2020-09-01 14:52:00 -0400

    Added figures and ey to ei...

--- Figure 1/fig1bdfhj.jl
+++ Figure 1/fig1bdfhj.jl
@@ -1,12 +1,12 @@
-by = 1.0;
-include("fig2params.jl")
+bi = 1.0;
+include("fig1params.jl")
 
 
 include("../Functions.jl")
 run_simulation()
 
 
-############ Figure 2b ############
+############ Figure 1b ############
 
 Np = N';
 
@@ -15,28 +15,28 @@ lbls2 = vcat("Infected", lbls);
 lbls2 = hcat(lbls2...);
 
 plot(Np[:,2:end], c=:blue, lw=0.4, alpha=0.4, title = "Number of infected and uninfected hosts",
-    xlabel = "Time", ylabel = "Number of individuals", label = lbls2, ylims =(0,70))
+    xlabel = "Time", ylabel = "Number of individuals", label = lbls2, ylims =(0,100))
 plot!(Np[:,1], c=:black, lw=0.4, label = "Uninfected")
 #plot!(sum(Np[:,2:end]; dims=2), label = "total parasites")
 
-# png("Figure 2/graph_2b.png")
+# png("Figure 1/graph_1b.png")
 
 
-########### Figure 2h #############
+########### Figure 1h #############
 
-#strains that survive at each time step for uy
+#strains that survive at each time step for ui
 survival = (Np.>0.0)[:,2:end];
-survived_ui = survival.*uy';
+survived_ui = survival.*ui';
 avg_survived = sum(survived_ui; dims=2)./sum(survival; dims=2);
 
-avg_w_survived = sum(Np[:,2:end].*uy'; dims=2)./sum(Np[:,2:end]; dims=2);
-uy_avg = avg_w_survived;
+avg_w_survived = sum(Np[:,2:end].*ui'; dims=2)./sum(Np[:,2:end]; dims=2);
+ui_avg = avg_w_survived;
 
-# plot(avg_survived, title = "Average uy in the population",xlabel = "Time", ylabel = "Mean mortality (ui)", leg = false)
+# plot(avg_survived, title = "Average ui in the population",xlabel = "Time", ylabel = "Mean mortality (ui)", leg = false)
 plot(avg_w_survived, c=:black,title = "Average mortality in the population",
     xlabel = "Time", ylabel = "Mean mortality (ui)", leg = false, ylims =(0,1))
 
-# png("Figure 2/graph_2h.png")
+# png("Figure 1/graph_1h.png")
 
 
 ############ Figure 2f ##############
@@ -52,31 +52,31 @@ avg_w_survived_βy = sum(Np[:,2:end].*βy'; dims=2)./sum(Np[:,2:end]; dims=2);
 
 #calculating R0
 k=1;
-H0 = c*βi_avg./uy_avg.*k.*(1-ux/bx);
+H0 = c*βi_avg./ui_avg.*k.*(1-ux/bx);
 
-H0_w = c*βy_avg./uy_avg.*k.*(1-ux/bx);
+H0_w = c*βy_avg./ui_avg.*k.*(1-ux/bx);
 
-V0 = by*ux./(bx*uy_avg);
+V0 = bi*ux./(bx*ui_avg);
 
 plot(V0,c=:black, title = "Average vertical transmission in the population", xlabel = "Time", ylabel = "Mean V0", leg = false, ylims=(0,1))
 
-# png("Figure 2/graph_2f")
+# png("Figure 1/graph_1f")
 
 
-######## Figure 2d ##########
+######## Figure 1d ##########
 
 R0 = H0 + V0;
 
 R0_w = H0_w + V0;
 
 # plot(R0, title = "Average R0 in the population", xlabel = "Time", ylabel = "Mean R0", leg = false, ylims=(0,))
-plot(R0_w,c=:black, title = "Average R0 in the population", xlabel = "Time", ylabel = "Mean R0", leg = false, ylims=(0,7))
+plot(R0_w,c=:black, title = "Average R0 in the population \nwith R0 = V0 +H0", xlabel = "Time", ylabel = "Mean R0", leg = false, ylims=(0,7))
 
-# png("Figure 2/graph_2d")
+# png("Figure 1/graph_1d")
 
 
 
-########## Figure 2j ###########
+########## Figure 1j ###########
 
 #calculating evenness
 function pielou(n)
@@ -88,4 +88,4 @@ end;
 ev = mapslices(pielou, Np[:,2:end]; dims=2);
 plot(ev, c=:black, title = "Evenness", xlabel = "Time", ylabel = "Relative abundance (log)", leg = false, ylims = (0,1))
 
-# png("Figure 2/graph_2j.png")
+# png("Figure 1/graph_1j.png")
