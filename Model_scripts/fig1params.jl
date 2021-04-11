70f622eaad26e9e262d2c687c7a6466cb473c2b5
kiristern <kiri.a.stern@gmail.com>
2021-04-11 15:25:12 -0400

    n_parasites 100->200 & format beta_y to better reflect article eqn

--- Model_scripts/fig1params.jl
+++ Model_scripts/fig1params.jl
@@ -2,27 +2,25 @@ using DifferentialEquations
 using Plots
 import Random
 
-
-n_parasites = 100;
+n_parasites = 200;
 
 #parameters
 c = 4.0;
 ux = 0.2;
-ux1 = fill(0.2, n_parasites); #le ux et ui 1000 à cause de la β # une autre façon :[0.2 for x in 1:1000]
-Random.seed!(1234);
+Random.seed!(4040);
 ui = rand(200:1000, n_parasites)/1000; #always >= ux
-u1 = (ui - ux1);
-βy = 3 * u1 ./ (u1.+ 1) # augmente avec la mortalité u
-bx = 1.0;
-ei = fill(0.9, n_parasites)   # constant for fig 2 part 1
+βy = 3 * (ui .- ux) ./ (ui .- ux .+ 1) # augmente avec la mortalité u
+bx = 1.0
+ei = fill(bx .- bi, n_parasites)
+
 
 debut = 0.0;
 duree = 1000.0;
 fin = debut + duree;
 N = zeros(Float64, (n_parasites+1, (n_parasites-1)*Int(duree)+1));
-parameters = (bx = bx, βy = βy, ei = ei, c = c, K = 99, ux = ux, bi = bi, ui = ui);
+parameters = (bx = bx, βy = βy, ei = ei, c = c, K = 100, ux = ux, bi = bi, ui = ui);
 
-Y = zeros(Float64, length(ei));
+Y = zeros(Float64, n_parasites);
 X0 = 10.0;
 new_U = vcat(X0, Y);
 Y[1] = 1.0;
