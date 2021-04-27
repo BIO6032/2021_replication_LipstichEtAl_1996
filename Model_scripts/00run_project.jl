c44532e2aa41694ab47216915561d0dcfbe3a627
kiristern <kiri.a.stern@gmail.com>
2021-04-27 13:08:31 -0400

    run all scripts

---
+++ Model_scripts/00run_project.jl
@@ -0,0 +1,6 @@
+include("Model_scripts/fig1acegi.jl")
+include("Model_scripts/fig1bdfhj.jl")
+include("Model_scripts/fig2acegi.jl")
+include("Model_scripts/fig2bdfhj.jl")
+include("Model_scripts/fig3acegi.jl")
+include("Model_scripts/fig3bdfhj.jl")
