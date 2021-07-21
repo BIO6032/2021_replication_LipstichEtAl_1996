7f100f16581a902966d4a15051a47f2b3c3cb083
kiristern <kiri.a.stern@gmail.com>
2021-07-20 21:57:35 -0400

    replace ncol->size (syntax)

--- Model_scripts/fig3bdfhj.jl
+++ Model_scripts/fig3bdfhj.jl
@@ -13,7 +13,7 @@ plot_population_numbers(
     hcat(labels...),
     "c=4.0\n \nNumber of infected and uninfected hosts",
     100,
-    ncol(Np),
+    size(Np)[1],
     "$figure_directory/graph_3b.png"
 )
 ########### Figure 3d ###########
@@ -26,15 +26,15 @@ H0_w = calculate_H0()
 # calculate the weighted V0
 V0_w = calculate_V0()
 # plot the average weighted R0
-plot_reproductive_rate(5, ncol(Np), "$figure_directory/graph_3d.png")
+plot_reproductive_rate(5, size(Np)[1], "$figure_directory/graph_3d.png")
 ########### Figure 3f ###########
 # plot the average V0
-plot_vertical_reproductive_ratio(1, ncol(Np), "$figure_directory/graph_3f.png")
+plot_vertical_reproductive_ratio(1, size(Np)[1], "$figure_directory/graph_3f.png")
 ########### Figure 3h ###########
 # calculate the average virulence
 vir_w_avg = calculate_average_virulence()
 # plot the average horizontal transmission & weighted virulence
-plot_horizontal_and_virulence(1, ncol(Np), "$figure_directory/graph_3h.png")
+plot_horizontal_and_virulence(1, size(Np)[1], "$figure_directory/graph_3h.png")
 ########### Figure 3j ###########
 # calculate & plot the population evenness through time
-plot_evenness(1, ncol(Np), "$figure_directory/graph_3j.png")
+plot_evenness(1, size(Np)[1], "$figure_directory/graph_3j.png")
